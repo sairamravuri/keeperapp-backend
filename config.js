447ccb4381854a00f3a3d889e05f3393a4f5c746
SAI RAM <sairamravuri123@gmail.com>
2023-07-14 17:15:02 +0530

    sairam

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
 const env = process.env;
-console.log(env.DB_PASSWORD);
 
 const config = {
   db: {
     /* don't expose password or any sensitive info, done only for demo */
-    url: "mysql -hcontainers-us-west-145.railway.app -uroot -p3Hza6ExvGZymsRi1STXj --port 6387 --protocol=TCP railway",
+    host: env.DB_HOST,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
+    multipleStatements: true,
   },
   mail: env.mailbaseURL,
   //listPerPage: env.LIST_PER_PAGE || 10,
